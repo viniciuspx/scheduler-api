338e3eacde760bc07cd9dc073f49dcef12ecf5a2
viniciuspx <vinicius.gabriel.px@gmail.com>
2024-01-25 11:32:37 -0300

    padronize routes and fix patch route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,9 +6,13 @@ import lists from "./lists";
 const router = express.Router();
 
 export default (): express.Router => {
-    authetication(router);
-    users(router);
-    lists(router);
-    
-    return router; 
-}
+  authetication(router);
+  users(router);
+  lists(router);
+
+  router.get("/", (req: express.Request, res: express.Response) => {
+    return res.status(200).json("Scheduler API UP");
+  });
+
+  return router;
+};
